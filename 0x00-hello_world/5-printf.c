a3937e61df585f5a7d9a1d6a7a646d05369d9a18
SukinaAmmar <sukinaammar453@gmail.com>
2022-12-08 12:18:31 -0800

    task4&5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * * main - entry point
+ *
+ * *
+ *
+ * * Return: Always 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		printf("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
