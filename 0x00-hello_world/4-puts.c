a63bf5cf9651d8755b727dbea35e14745cebe087
SukinaAmmar <sukinaammar453@gmail.com>
2022-12-09 07:56:41 -0800

    task444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - entry point
  *
  * Return: Always 0 (Success)
@@ -12,5 +12,5 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+			return (0)
 }
