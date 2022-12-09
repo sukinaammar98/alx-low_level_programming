42848fc1a8575b31b10697b89062ac64a1e845ac
SukinaAmmar <sukinaammar453@gmail.com>
2022-12-09 08:03:53 -0800

    jdjod

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-/*
- * main - entry point
+/**
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
