f406321185cc86d23e959a308d9f1d6b71618c3f
SukinaAmmar <sukinaammar453@gmail.com>
2022-12-09 07:44:58 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-/**
+/*
+ * main - entry point
  *
- * * main - entry point
- *
- * *
- *
- * * Return: Always 0
- *
- * */
+ * Return: Always 0
+ */
 
 int main(void)
 
@@ -17,5 +13,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  * * main - entry point
@@ -8,14 +7,10 @@
  *
  * * Return: Always 0
  *
- * */
-
+ */
 int main(void)
 
 {
-
 		printf("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
